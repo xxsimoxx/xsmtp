3f2244c07d1dca823484b228cec4ad5ddc2e613d
xxsimoxx <simone@gieffeedizioni.it>
2025-02-25 17:14:42 +0100

    Make kses more readable

--- traits/Helpers.trait.php
+++ traits/Helpers.trait.php
@@ -23,11 +23,16 @@ trait Helpers
 
 
 	function add_notice($transient, $message, $failure = false) {
+		$kses_allowed = [
+			'br' => [],
+			'i'  => [],
+			'b'  => [],
+		];
 		$other_notices = get_transient($transient);
 		$notice = $other_notices === false ? '' : $other_notices;
 		$failure_style = $failure ? 'notice-error ' : 'notice-success ';
 		$notice .= '<div class="notice '.$failure_style.'is-dismissible">';
-		$notice .= '    <p>'.wp_kses($message, ['br' => [], 'i' => [], 'b' => [],]).'</p>';
+		$notice .= '    <p>'.wp_kses($message, $kses_allowed).'</p>';
 		$notice .= '</div>';
 		set_transient($transient, $notice, \HOUR_IN_SECONDS);
 	}
